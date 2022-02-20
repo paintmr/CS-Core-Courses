ea8538bfae0a09dceaadba99a80f509c8416a8ba
L <chujingxl@gmail.com>
2022-02-20 23:38:03 +0800

    CS61B - Week3 - 4.1 reading, video, slides, guide

--- 3 Berkeley CS61B Data Structures Spring 2021/Week 3/4.1 Reading/2 Interface Inheritance/List61B.java
+++ 3 Berkeley CS61B Data Structures Spring 2021/Week 3/4.1 Reading/2 Interface Inheritance/List61B.java
@@ -15,9 +15,13 @@ public interface List61B<Item> {
 
   public Item removeLast();
 
-  default public void print() {
+  default public void print() { // Remember to write default. Everything that implements the List61B class can
+                                // use this print() method, as long as they do not override print().
     for (int i = 0; i < size(); i++) {
-      System.out.print(get(i) + " ");
+      System.out.print(get(i) + " "); // get has to seek all the way to the given item for SLists, so it is not
+                                      // efficient for an SLList. Therefore, the SLList has to override it to make it
+                                      // more efficient. But this is efficient for an AList, so an AList does
+                                      // not have to override print().
     }
     System.out.println("");
   }
